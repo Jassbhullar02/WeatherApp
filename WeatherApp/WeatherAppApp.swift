4bd6cf79a0081554578c5fa67977a4f925230299
Jaspreet Bhullar <jassbhullar1502@gmail.com>
2025-03-30 14:37:41 +0530

    Add WeatherApp project

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jaspreet Bhullar on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
